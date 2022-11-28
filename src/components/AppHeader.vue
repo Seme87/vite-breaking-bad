a42f74b485c691212cd61e20238c57cf2501bb36
Seme87 <massimomonteleone@hotmail.com>
2022-11-28 16:53:54 +0100

    aggiuge struttura e componenti del progetto

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header>
+    <div class="logo-title d-flex align-items-start p-3">
+      <div class="logo col-1 align-self-end">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <h2 class="col align-self-end mx-2">Breaking Bad Api</h2>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+img {
+  max-width: 100%;
+}
+.logo {
+  height: 50px;
+}
+.logo-title {
+  color: white;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "appMain",
+};
+</script>
+
+<template>
+  <div>Ciao</div>
+</template>
+
+<style lang="scss" scoped></style>
